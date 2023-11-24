46790e6d02a01cbf8566e1910f3508d5d7a872cd
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2023-11-24 14:16:36 +0200

    feat: add fake data

---
+++ src/products.js
@@ -0,0 +1,60 @@
+import product1 from './assets/01.png';
+import product2 from './assets/02.png';
+import product3 from './assets/03.png';
+import product4 from './assets/04.png';
+import product5 from './assets/05.png';
+import product6 from './assets/06.png';
+import product7 from './assets/07.png';
+import product8 from './assets/08.png';
+
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: 'Samsung Galaxy S21',
+        price: 899.0,
+        productImage: product1,
+    },
+    {
+        id: 2,
+        productName: 'Dell XPS 13',
+        price: 1499.0,
+        productImage: product2,
+    },
+    {
+        id: 3,
+        productName: 'Sony A6400 Camera',
+        price: 799.0,
+        productImage: product3,
+    },
+    {
+        id: 4,
+        productName: "Levi's Classic Denim Jacket",
+        price: 249.0,
+        productImage: product4,
+    },
+    {
+        id: 5,
+        productName: 'Smart LED Bulbs',
+        price: 14.99,
+        productImage: product5,
+    },
+    {
+        id: 6,
+        productName: 'Athletic Performance Tee',
+        price: 49.0,
+        productImage: product6,
+    },
+    {
+        id: 7,
+        productName: 'URBAN EXPLORER JACKET',
+        price: 79.0,
+        productImage: product7,
+    },
+    {
+        id: 8,
+        productName: 'VINTAGE EMBROIDERED HAT',
+        price: 29.0,
+        productImage: product8,
+    },
+];
