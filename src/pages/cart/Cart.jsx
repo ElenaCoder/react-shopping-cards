86c4f9d6b246ccfeacd70c75505af7adb43aa816
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2023-11-27 16:36:56 +0200

    feat: add totalAmount result to Cart component

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,11 +1,12 @@
-import React, {useContext} from 'react';
+import React, { useContext } from 'react';
 import { PRODUCTS } from '../../products';
 import { ShopContext } from '../../context/ShopContext';
 import CartItem from './CartItem';
 import './Cart.css';
 
 const Cart = () => {
-    const { cartItems } = useContext(ShopContext);
+    const { cartItems, getTotalCartAmount } = useContext(ShopContext);
+    const totalAmount = getTotalCartAmount();
 
     return (
         <div className='cart'>
@@ -15,9 +16,15 @@ const Cart = () => {
             <div className='cartItems'>
                 {PRODUCTS.map((product) => {
                     if (cartItems[product.id] !== 0) {
-                        return <CartItem key={product}  data={product}/>;
+                        return <CartItem key={product} data={product} />;
                     }
                 })}
+            </div>
+
+            <div className='checkout'>
+                <p>Subtotal: ${totalAmount}</p>
+                <button> Continue Shopping</button>
+                <button> Checkout</button>
             </div>
         </div>
     );
