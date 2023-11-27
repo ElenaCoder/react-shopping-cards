569f8dcb96e11a4d5a40af414f2660ff5cdad91a
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2023-11-27 16:09:12 +0200

    feat: implement Cart component

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, {useContext} from 'react';
+import { PRODUCTS } from '../../products';
+import { ShopContext } from '../../context/ShopContext';
+import CartItem from './CartItem';
+import './Cart.css';
 
 const Cart = () => {
-  return (
-    <div>Cart</div>
-  )
-}
+    const { cartItems } = useContext(ShopContext);
+
+    return (
+        <div className='cart'>
+            <div>
+                <h1>Your Cart Items</h1>
+            </div>
+            <div className='cartItems'>
+                {PRODUCTS.map((product) => {
+                    if (cartItems[product.id] !== 0) {
+                        return <CartItem key={product}  data={product}/>;
+                    }
+                })}
+            </div>
+        </div>
+    );
+};
 
 export default Cart;
